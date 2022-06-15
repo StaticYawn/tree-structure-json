713549c4ebc42c45a7f857c8a32f378d0eec62c3
Yawny_work <yawnyyboy@gmail.com>
2022-06-15 13:39:53 +0200

    adjust fetch for parallel fetch

--- src/fetchRequests.ts
+++ src/fetchRequests.ts
@@ -1,16 +1,18 @@
 import { ActivityData, ProjectData, TicketData } from "./types";
 
 async function getJson(url: string) {
-    const response = await fetch(url);
-    if (response.ok) {
-        const data = await response.json();
-        return data;
+    try {
+        const response = await fetch(url);
+        if (!response.ok) throw 'shit is indeed fucked'
+        return response.json();
+
+    } catch (e) {
+        console.error(e)
     }
-    // throw new Error('blah!')
 }
-
-const ticketsJson: Array<TicketData> = await getJson('http://127.0.0.1:3000/tickets.json');
-const projectsJson: Array<ProjectData> = await getJson('http://127.0.0.1:3000/projects.json');
-const activiesJson: Array<ActivityData> = await getJson('http://127.0.0.1:3000/activities.json');
-
-export { ticketsJson, projectsJson, activiesJson };
+export const [ticketsJson, projectsJson, activitiesJson]:
+    [TicketData[], ProjectData[], ActivityData[]] = await Promise.all([
+        getJson('http://127.0.0.1:3000/tickets.json'),
+        getJson('http://127.0.0.1:3000/projects.json'),
+        getJson('http://127.0.0.1:3000/activities.json')
+    ]);
