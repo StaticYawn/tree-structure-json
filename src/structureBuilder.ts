41599482014d59bd64f345e8f2c96e66e5c89892
Yawny_work <yawnyyboy@gmail.com>
2022-06-15 17:01:26 +0200

    formatting

--- src/structureBuilder.ts
+++ src/structureBuilder.ts
@@ -7,24 +7,25 @@ const findAll = (obj: Array<ActivityData>, value: number): Array<ActivityData> =
 
 export function h(tagName: string, attrsAndEvents: { [key: string]: string | ((event: Event) => void) } = {}, ...children: (Element | string)[]): Element {
     const element = document.createElement(tagName);
-    
+
     const [attributes, events] = partition(
-        Object.entries(attrsAndEvents),
-        (([key]) => !key.startsWith('on'))
-    )
+        Object.entries(attrsAndEvents), ([key]) => !key.startsWith('on')
+    );
+
     for (const [eventRaw, handlers] of events) {
         const event = eventRaw.slice(2)
 
-        castArray(handlers as keyof EventListenerOrEventListenerObject).forEach(handler => element.addEventListener(event, handler))
+        castArray(handlers as keyof EventListenerOrEventListenerObject)
+            .forEach(handler => element.addEventListener(event, handler))
     }
     Object.assign(element, Object.fromEntries(attributes));
     element.append(...children);
     return element;
 }
 
-function castArray<T>(item:T|T[]):T[] {
+function castArray<T>(item: T | T[]): T[] {
     return Array.isArray(item) ? item : [item]
-  }
+}
 
 function partition<T>(iter: Iterable<T>, fn: (item: T, index: number, iter: Iterable<T>) => unknown): [T[], T[]] {
     const passed = []
