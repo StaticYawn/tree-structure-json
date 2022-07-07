cfd6a228db4ae5111620c1d0856b4603a8c2fb5f
Yawny_work <yawnyyboy@gmail.com>
2022-07-07 17:11:31 +0200

    adjusted for css

--- src/constantGlobals.ts
+++ src/constantGlobals.ts
@@ -1,22 +1,22 @@
 export const SYS = {
-    1: { text: 'Arbeitsbeginn', color: 'green' },
+    1: { text: 'Arbeitsbeginn', color: 'has-text-success' },
 
-    2: { text: 'Projektwechsel', color: 'gray' },
+    2: { text: 'Projektwechsel', color: 'has-text-gray' },
 
-    3: { text: 'Pausenende', color: 'gray' },
+    3: { text: 'Pausenende', color: 'has-text-gray' },
 
-    4: { text: 'Pausenbeginn', color: 'gray' },
+    4: { text: 'Pausenbeginn', color: 'has-text-gray' },
 
     5: { text: 'Activity', color: '', },
 
-    6: { text: 'Arbeitsende', color: 'red' }
+    6: { text: 'Arbeitsende', color: 'has-text-danger' }
 }
 export const ICONS = {
-    note: 'fas fa-message',
-    image: 'fas fa-image',
+    note: 'fas fa-message has-text-success',
+    image: 'fas fa-image has-text-info',
     plus: 'fas fa-plus',
     minus: 'fas fa-minus',
-    edit: 'fas fa-edit',
+    edit: 'fas fa-pencil-alt',
     delete: 'fas fa-trash'
 }
 
